ef0276c8e6202f0fac21ebcbfc68b4f5936c6800
Nick Hanselman <nickhanselman@icloud.com>
2018-04-28 23:40:11 -0600

    general structure for hasEventOccurred

--- event.c
+++ event.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "event.h"
+
+void hasEventOccurred(const char * noun) {
+    if (!strcmp(noun, "bookshelf")) {
+        
+    } else if (!strcmp(noun, "robot")) {
+
+    } else if (!strcmp(noun, "desk")) {
+
+    } else {
+        return;
+    }
+}
+
+
+
+
 
 
 
