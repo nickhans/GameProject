cd1c55aea0557060720d68e997f9cecd8a25595d
Nick Hanselman <nickhanselman@icloud.com>
2018-04-29 00:19:17 -0600

    changed wording in response to events

--- event.c
+++ event.c
@@ -39,18 +39,19 @@ bool eventProcessing(const char * noun) {
     if (!strcmp(noun, "bookshelf")) {
         if (book->locationOfObject == bookshelf->containInventory) {
             circle->locationOfObject = bookshelf->locationOfContainer;
-            printf("A %s falls of of the bookshelf into the center of the room.\n", circle->objDescription);
+            printf("%s falls off of the bookshelf into the center of the room.\n", circle->objDescription);
         }
     } else if (!strcmp(noun, "robot")) {
         if (battery->locationOfObject == robot->containInventory) {
             square->locationOfObject = robot->locationOfContainer;
             printf("The robot powers on and moves across the room revealing\n"
-            "a %s on the floor.\n", square->objDescription);
+            "%s on the floor.\n", square->objDescription);
         }
     } else if (!strcmp(noun, "desk")) {
         if (chair->locationOfObject == desk->containInventory) {
             triangle->locationOfObject = desk->locationOfContainer;
-            printf("As you slide the chair into the desk a %s falls\nfrom under the chair onto the floor.", triangle->objDescription);
+            printf("As you slide the chair into the desk %s falls\n"
+            "from under the chair onto the floor.\n", triangle->objDescription);
         }
     } else if (!strcmp(noun, "trapdoor")) {
         if (didWin()) {
